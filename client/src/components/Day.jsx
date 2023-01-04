50b21ae4a5f28e5c625e26b79d8c36fd6899e368
Annabelle Peacock <aspeacock92@gmail.com>
2023-01-04 12:12:18 +0000

    addsymptoms function should now work

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -5,30 +5,58 @@ import React from "react";
 
 const Day = () => {
 
-    // List out possible symptoms, eventually as icon buttons. Then onChange() we map() through the selected symptoms to create a new array and save it to the day
-    const possibleSymptoms = ["sneezing and coughing", "runny or blocked nose", "itchy, red or watery eyes", "itchy throat, mouth, nose, ears", "loss of smell", "pain around temple or forehead", "headache", "earache", "tiredness"]
+    const possibleSymptoms = [
+        {
+            name: "sneezing and coughing"
+        },
+        {
+            name: "runny or blocked nose"
+        },
+        {
+            name: "itchy, red or watery eyes"
+        },
+        {
+            name: "itchy throat, mouth, nose, ears"
+        },
+        {
+            name: "loss of smell"
+        }, 
+        {
+            name: "pain around temple or forehead"
+        },
+        {
+            name: "headache"
+        },
+        {
+            name: "earache"
+        }, 
+        {
+            name: "tiredness"
+        }];
+
+    let symptom = possibleSymptoms[0];
+    
+    const listSymptoms = possibleSymptoms.map((symptom) => 
+        <li key={symptom.id}>
+            {symptom.name}
+        </li>
+    ); 
+
 
     const addSymptoms = () => {
         const userSymptoms = window.localStorage("userSymptoms");
 
         let newUserSymptoms = [...userSymptoms];
 
-        newUserSymptoms.push(possibleSymptoms);
+        newUserSymptoms.push(symptom);
 
         localStorage.setItem("userSymptoms", newUserSymptoms);
     };
 
-
-
-    // then if the day has +1 in [] array, it gets coded a colour on the calendar
-    
-
     return (
         <ul>
-            <li>
-                {possibleSymptoms}
-                <button onClick={addSymptoms}>Add symptom</button>
-            </li>
+            {listSymptoms}
+                <li key={symptom.id}><button onClick={addSymptoms}>Add symptom</button></li>
         </ul>
         
     )

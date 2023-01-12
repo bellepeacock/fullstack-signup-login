61439230acfdcf0731ea235afec39b2f3c8c9c4e
Annabelle Peacock <aspeacock92@gmail.com>
2023-01-12 12:49:29 +0100

    added form, need to have each item map through possibleSymptoms

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -1,9 +1,10 @@
-// want to see / edit / update / delete symptoms!
-
 import React from "react";
+// import { useState, useEffect } from "react";
 
 
-const Day = () => {
+export const Day = () => {
+
+    // const [userSymptoms, setUserSymptoms] = useState([]);
 
     const possibleSymptoms = [
         {
@@ -36,30 +37,81 @@ const Day = () => {
 
     let symptom = possibleSymptoms[0];
     
-    const listSymptoms = possibleSymptoms.map((symptom) => 
-        <li key={symptom.id}>
-            {symptom.name}
-        </li>
-    ); 
+    // const listSymptoms = possibleSymptoms.map((s) => 
+    //     <div>
+    //         <input type="checkbox" value={s.name} />
+    //         <label>{s.name}</label>
+    //     </div>
+    // ); 
 
+    // useEffect(() => {
+    //     localStorage.setUserSymptoms('userSymptoms', JSON.stringify(userSymptoms));
+    //   }, [userSymptoms]);
 
-    const addSymptoms = () => {
-        const userSymptoms = window.localStorage("userSymptoms");
+    // const userSymptoms = localStorage.getItem("userSymptoms");
 
+    const userSymptoms = [];
+    
+    const addSymptom = () => {
         let newUserSymptoms = [...userSymptoms];
 
         newUserSymptoms.push(symptom);
 
-        localStorage.setItem("userSymptoms", newUserSymptoms);
+        localStorage.setItem("userSymptoms", JSON.stringify(newUserSymptoms));
     };
 
+    console.log(userSymptoms);
+
+    // const listUserSymptoms = userSymptoms.map((s) => 
+    //     <div>
+    //         <input type="checkbox" value={s.name} />
+    //         <label>{s.name}</label>
+    //     </div>
+
+    // );
+    console.log(userSymptoms.length === 0 ? true : false );
+    console.log(userSymptoms);
+
+        // display one checkbox, see how that works [x]
+        // then add the others with the loop [x]
+        // make it into a form, with a submit button (add symptom) and call the function [ ]
+        // want to save these userSymptoms in the db as on this day, this user had xyz [ ]
+        // when the page loads, go to DB and see for this user, this day, what were the symptoms? [ ]
+                // Can always see the entire list to add more, but can see which they've already selected: selected is true
+                // can format the checkbox to be an image (later on!)
+
+        // ^^define the behaviours of the pg really clearly to help code the pg!
+        // draw on paper the final page will look like!! List out all the actions the user can take, e.g. click on this list, log in, —for each action, what is the behaviour you can do [x] 
+            // add to the trello board
+            // msg Lakshmi on Slack next wk with it done!! [x]
+
     return (
-        <ul>
-            {listSymptoms}
-                <li key={symptom.id}><button onClick={addSymptoms}>Add symptom</button></li>
-        </ul>
-        
-    )
-}
+        <div>
+            <form className="container">
+                <label className="container" onClick={addSymptom()}>
+                    <input type="checkbox" />
+                    <img className="checkmark" alt="labrador0.1" src="https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_1280.jpg" /> 
+                </label>
+
+                <label className="container" onClick={addSymptom()}>
+                    <input type="checkbox" />
+                    <img className="checkmark" alt="labrador" src="https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_1280.jpg" /> 
+                </label>
+
+                <label className="container" onClick={addSymptom()}>
+                    <input type="checkbox"  />
+                    <img className="checkmark" alt="labrador1" src="https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_1280.jpg" /> 
+                </label>
+
+                <label className="container" onClick={addSymptom()}>
+                    <input type="checkbox" />
+                    <img className="checkmark" alt="labrador2" src="https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_1280.jpg" />   
+                </label>
+
+            </form>
+        </div>
+)
+};
+
 
 export default Day;

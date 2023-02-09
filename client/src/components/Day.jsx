a5c89d94270b4ba6bd0f83f79639d29d215dbda2
Annabelle Peacock <aspeacock92@gmail.com>
2023-02-09 18:27:38 +0100

    checkboxes received with flase values

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 
 export const Day = () => {
-
-    const [userSymptoms, setUserSymptoms] = useState([]);
 
     const possibleSymptoms = [
         {
@@ -35,22 +33,50 @@ export const Day = () => {
             name: "tiredness"
         }];
 
-    let symptom = possibleSymptoms[0];
+        // let symptom = possibleSymptoms[0];
 
+        const [userSymptoms, setUserSymptoms] = useState(
+          new Array(possibleSymptoms.length).fill(false)
+        );
+        // const [checkedState, setCheckedState] = useState(
+        //     new Array(possibleSymptoms.length).fill(false)
+        //   );
 
-    // const userSymptoms = [];
-    
-    const addSymptom = () => {
-        let setUserSymptoms = [...userSymptoms];
+          const handleOnChange = (position) => {
+            const updatedUserSymptoms= possibleSymptoms.map((item, index) =>
+              index === position ? !item : item
+            );
 
-        setUserSymptoms.push(symptom);
+            setUserSymptoms(updatedUserSymptoms);
+          localStorage.setItem("userSymptoms", JSON.stringify(updatedUserSymptoms));
 
-        localStorage.setItem("userSymptoms", JSON.stringify(setUserSymptoms));
-    };
+          }
+
+    // const addSymptom = () => {
+    //     let newUserSymptoms = [...userSymptoms];
+
+    //     newUserSymptoms.push(symptom);
+
+    //     //change to a remote DB after local storage working!
+    //     localStorage.setItem("userSymptoms", JSON.stringify(newUserSymptoms));
+    // };
     console.log(userSymptoms);
+
+    // const handleSymptomChange = (e) => {
+    //     let newUserSymptoms = [...userSymptoms];
+    //     console.log("handleSymptomChange:");
+    //     console.log(newUserSymptoms);
+    //     console.log(e.target.value);
+    //     // newUserSymptoms.push(e.target.value);
+
+    //     // this.setState({userSymptoms: newUserSymptoms});
+      
+    //  };
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        console.log(event.target.symptoms.value);
+        // addSymptom(setUserSymptoms);
         console.log(userSymptoms)
     };
 
@@ -58,25 +84,49 @@ export const Day = () => {
     console.log(userSymptoms);
 
 
-    return ( 
+     return ( 
+    //   <div className="App">
+    //   <h3>Select Toppings</h3>
+    //   <ul className="symptoms-list">
+    //     {possibleSymptoms.map(({ name }, index) => {
+    //       return (
+    //         <li key={index}>
+    //           <div className="symptoms-list-item">
+    //             <div className="left-section">
+    //               <input
+    //                 type="checkbox"
+    //                 id={`custom-checkbox-${index}`}
+    //                 name={possibleSymptoms.name}
+    //                 value={possibleSymptoms.name}
+    //                 checked={checkedState[index]}
+    //                 onChange={() => handleOnChange(index)}
+    //               />
+    //               <label htmlFor={`custom-checkbox-${index}`}>{possibleSymptoms.name}</label>
+    //             </div>
+    //           </div>
+    //         </li>
+    //       );
+    //     })}
+    //   </ul>
+    // </div>
+
         <div>
-            <form onSubmit={handleSubmit} addSymptom={addSymptom}>
+            <form onSubmit={handleSubmit} >
             {possibleSymptoms.map((s) => (
                 <ul>
                     <li key={s}>
-                        <label className="container" onClick={addSymptom()}>
-                            <input type="checkbox" />
+                        <label className="container" onClick={setUserSymptoms}>
+                            <input type="checkbox" name="symptoms" checked={userSymptoms[userSymptoms.index]} onChange={() => handleOnChange(userSymptoms.index)} />
                             <textarea value={s.name} />
-                            <img className="checkmark" alt="labrador0.1" src="https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_1280.jpg" /> 
+                            {/* <img className="checkmark" alt="labrador0.1" src="https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_1280.jpg" />  */}
                         </label>
                     </li>
                 </ul>
             ))}
+             <button>Submit</button>
             </form>
-
         </div>
 )
 };
-
 
 export default Day;

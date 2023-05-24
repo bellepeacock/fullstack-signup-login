4d3bc30323deb58930466b5283e790cc85bf53e4
Annabelle Peacock <aspeacock92@gmail.com>
2023-05-24 15:22:27 +0200

    checkbox working, code cleaned up, need to now save symptoms to the user in the database

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -1,105 +1,88 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../css/app.css";
 import "../css/checklist.css"
 
 
 const Day = () => {
-
+      
+    // list of all the symptoms
     const possibleSymptoms = [
-        {
-            name: "sneezing and coughing",
-            // selected: false
-        },
-        {
-            name: "runny or blocked nose",
-            // selected: false
-        },
-        {
-            name: "itchy, red or watery eyes",
-            // selected: false
-        },
-        {
-            name: "itchy throat, mouth, nose, ears",
-            // selected: false
-        },
-        {
-            name: "loss of smell",
-            // selected: false
-        }, 
-        {
-            name: "pain around temple or forehead",
-            // selected: false
-        },
-        {
-            name: "headache",
-            // selected: false
-        },
-        {
-            name: "earache",
-            // selected: false
-        }, 
-        {
-            name: "tiredness",
-            // selected: false
-        }];
-
-        // let s = possibleSymptoms[0];
-
-        // const [checkedState, setCheckedState] = useState(
-        //     new Array(possibleSymptoms.length).fill(false)
-        // );
-
-        const [checkedState, setCheckedState] = useState(false);
-
-        const [userSymptoms, setUserSymptoms] = useState([]);
-
-        //loop over the checkedState array using the array map method. 
-        //If the value of the passed position parameter matches with the current index, then we reverse its value. Then, if the value is true it will be converted to false using !item and if the value is false, then it will be converted to true.
-        const handleOnChange = (s, value, position) => {
-                let symptoms = [ ...userSymptoms];
+        { id: 0, name: "sneezing and coughing", checked: false },
+        { id: 1, name: "runny or blocked nose", checked: false },
+        { id: 2, name: "itchy, red or watery eyes", checked: false },
+        { id: 3, name: "itchy throat, mouth, nose, ears", checked: false },
+        { id: 4, name: "loss of smell", checked: false },
+        { id: 5, name: "pain around temple or forehead", checked: false },
+        { id: 6, name: "headache", checked: false },
+        { id: 7, name: "earache", checked: false },
+        { id: 8, name: "tiredness", checked: false }
+    ];
+    
+    // state to keep track of user selected symptoms on the selected day 
+    // initialised to possible symptoms with nothing checked
+    const [userSymptoms, setUserSymptoms] = useState(possibleSymptoms);
   
-                if (symptoms.includes(value)) {
-                  symptoms = symptoms.filter( (s) => s !== value)
-                } else {
-                  symptoms.push(value);
-                }
-                setUserSymptoms(symptoms);
 
-                console.log(symptoms);
-                console.log(userSymptoms);
-        };
+    // to see changes in state after they have taken effect
+    // state changes in react are done async, so they might not be available immediately for actions like console.log
+    useEffect(() => {
+        console.log("User Symptoms: ", userSymptoms);
+    });
 
-        // function working but not updating immediately for symptoms, not updating userSymptoms ^^
+          
+    // handler to update user symptoms when logged in user checks / unchecks a symptom
+    const handleToggleUserSymptoms = (symptomName, isChecked) => {
+        setUserSymptoms( userSymptoms.map( s => {
+            if (s.name == symptomName) {
+                // update the state with the current status (checked or unchecked)
+                return { ...s, checked: isChecked};
+            } else {
+                // no changes
+                return s;
+            }
+        }));
+    };
 
+     // handler to save user symptoms to DB when the user clicks on Save
+     const saveUserSymptoms = () => {
+        // write code to save the user symptoms into the DB
+        // can directly access all the selected user symptoms from the state and save them
+    };
 
-     return ( 
+    return (
         <div className="App home">
-        <h3>Select symptoms</h3>
-        <form>
-        <ul className="symptoms-list checklist">
-          {possibleSymptoms.map(({ name }, index) => {
-            return (
-              <li key={index}>
-                <div className="possible-symptoms-list">
-                    <input
-                      type="checkbox"
-                      id={`custom-checkbox-${index}`}
-                      name={name}
-                      value={name}
-                      checked={checkedState[index]}
-                      onClick={() => handleOnChange(index, name)}
-                    />
-                    <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-        <input type="submit" value="Submit" />
-        </form>
-      </div>
-)
-};
+            <h3>Select symptoms</h3>
+            <form>
+                <ListSymptoms 
+                    items={userSymptoms}
+                    onToggle={handleToggleUserSymptoms}/>
+                <input type="button" value="Save" onClick={() => saveUserSymptoms()}/>
+            </form>
+        </div>
+    );
+
+    function ListSymptoms({ items, onToggle }) {
+        return (
+            <ul>
+                {items.map(item => (
+                        <li key={item.id}>
+                            <div className="possible-symptoms-list">
+                                <input
+                                    type="checkbox"
+                                    id={`custom-checkbox-${item.id}`}
+                                    name={item.name}
+                                    value={item.name}
+                                    checked={item.checked}
+                                    onChange={e => onToggle(item.name, e.target.checked)}    
+                                    />
+                                <label htmlFor={`custom-checkbox-${item.id}`}>{item.name}</label>
+                            </div>
+                        </li>
+                    ))}
+            </ul>
+        )
+    };
+}
 
 export default Day;
